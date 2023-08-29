36562eebde5855567998aeadde12c1b20eaa3953
trinitytrin <choudhury.trina@gmail.com>
2023-08-30 04:20:42 +0600

    Styling: Clean-up Game Cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -21,7 +21,7 @@ const GameGrid = ({ gameQuery }: GameGridProps) => {
         <>
             {error && <Text>{error}</Text>}
 
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding='10' spacing={5}>
+            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding='10' spacing={6}>
                 {isLoading && skeletons.map(skeleton => <GameCardContainer key={skeleton}>
                     <GameCardSkeleton />
                 </GameCardContainer>)}
