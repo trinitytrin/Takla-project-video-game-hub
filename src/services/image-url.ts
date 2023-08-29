f04b9c18f9d63314c7f5cdf17801af859a762049
trinitytrin <choudhury.trina@gmail.com>
2023-08-30 02:05:32 +0600

    Added NoImage Placeholders for games with No Image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,9 @@
 
+import imagePlaceHolder from '../assets/no-image-placeholder-6f3882e0.webp';
+
 const getCroppedImageUrl = (url : string) => {
 
-    if(!url) return '';
+    if(!url) return imagePlaceHolder;
 
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
